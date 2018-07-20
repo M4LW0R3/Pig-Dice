efd895de656787f749170ba627b3037cef3acc10
youngkhalid <youngkhalifah98@gmail.com>
2018-07-20 14:44:10 +0300

    styles and css modifications

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".startInput form").submit(function(event){
+        event.preventDefault();
+
+
+        var player1 = $("#player1").val();
+    var player2 = $("#player2").val();
+          $(this).hide();
+    })
+})
+ 
