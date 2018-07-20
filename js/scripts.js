89b49e40cd2ee535c484d05ea44ba2d1bffa8134
youngkhalid <youngkhalifah98@gmail.com>
2018-07-20 18:03:15 +0300

    font changes made

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,25 @@ $(document).ready(function(){
 
 
         var player1 = $("#player1").val();
-    var player2 = $("#player2").val();
+        var player2 = $("#player2").val();
           $(this).hide();
+
+        playerOne = new player1(player1,true);
+        playerTwo = new player2(player2,false);        
+
+    });
+    
+    $(".roll").click(function(){
+        if(playerOne.turn == true){
+
+            if(playerOne.totalRoll >=100){
+                alert(playerOne.playerName + "Congrats to the Winner!");
+                playerOne.reset()
+            }else{
+                player
+            }
+        }
     })
+
 })
  
